56be964f832d64c23a4664cc765ce16cb4aaffbd
Your Name <you@example.com>
2023-09-18 00:52:23 +0300

    add sliding animation

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,22 +1,51 @@
 import 'package:books_app/core/utils/assets.dart';
+import 'package:books_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds:1 ),
+    );
+
+    slidingAnimation= Tween<Offset>(begin:const Offset(0, 2) ,end:Offset.zero )
+      .animate(animationController);
+      animationController.forward();
+      
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment:CrossAxisAlignment.stretch ,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Center(
-          child: Text('Read Free Books',style: TextStyle(
-           fontSize: 16
-          ),),
+         Center(
+          child: SlidingText(slidingAnimation: slidingAnimation),
         )
       ],
     );
   }
 }
+
