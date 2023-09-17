3a13003df1a8ef829e59bd7a65b9b12d5461b41e
Your Name <you@example.com>
2023-09-18 01:23:18 +0300

    .

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const  kprimaryBGcolor = Color(0xff100B20);
+const ktransitionDuration =Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -18,16 +18,12 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     super.initState();
 
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds:1 ),
-    );
-
-    slidingAnimation= Tween<Offset>(begin:const Offset(0, 2) ,end:Offset.zero )
-      .animate(animationController);
-      animationController.forward();
-      
+    {
+      initSlidingAnimation();
+     
+    }
   }
+
   @override
   void dispose() {
     super.dispose();
@@ -41,11 +37,22 @@ class _SplashViewBodyState extends State<SplashViewBody>
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-         Center(
+        Center(
           child: SlidingText(slidingAnimation: slidingAnimation),
         )
       ],
     );
   }
-}
 
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+}
