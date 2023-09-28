8bf828a210dd3449b1b0f948bcfae4b6ab981ab3
Your Name <you@example.com>
2023-09-28 17:23:43 +0300

    cashed network image

--- lib/features/home/presentation/view/widgets/custom_book_cover.dart
+++ lib/features/home/presentation/view/widgets/custom_book_cover.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookCover extends StatelessWidget {
@@ -6,13 +7,15 @@ class CustomBookCover extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-                image: NetworkImage(imageURL), fit: BoxFit.fill)),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(imageUrl: imageURL,
+        fit: BoxFit.fill,
+        errorWidget: (context, url, error) => const Center(child: Icon(Icons.access_alarm)),
+        
+        )
       ),
     );
   }
