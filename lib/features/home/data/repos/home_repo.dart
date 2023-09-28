6c256300e7a83058bbea3b77b379e62012fef832
Your Name <you@example.com>
2023-09-28 09:03:15 +0300

    create server faileur

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -4,6 +4,6 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
 
- Future<Either<Faileur,List<BooksModel>>> featchBestSellerBooks();
- Future<Either<Faileur,List<BooksModel>>> featchFeaturedBooks();
+ Future<Either<Failure,List<BooksModel>>> featchNewestBooks();
+ Future<Either<Failure,List<BooksModel>>> featchFeaturedBooks();
 }
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,17 +1,35 @@
+import 'package:books_app/core/utils/api_services.dart';
 import 'package:books_app/core/utils/errors/faileur.dart';
 import 'package:books_app/features/home/data/models/book_model/book_model.dart';
 import 'package:books_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImp implements HomeRepo{
+
+final ApiServices apiServices;
+
+  HomeRepoImp({required this.apiServices});
   @override
-  Future<Either<Faileur, List<BooksModel>>> featchBestSellerBooks() {
-    // TODO: implement featchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BooksModel>>> featchNewestBooks() async{
+   var data= await apiServices.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:computer science');
+
+  try {
+  List<BooksModel> books=[];
+  for (var item in data['items']) {
+    books.add(BooksModel.fromJson(item));
+  }
+  return right(books);
+} catch (e) {
+  if(e is DioException){
+    return left(ServerFailure.fromDioException(e));
+  
+  }return left(ServerFailure(e.toString()));
+}
   }
 
   @override
-  Future<Either<Faileur, List<BooksModel>>> featchFeaturedBooks() {
+  Future<Either<Failure, List<BooksModel>>> featchFeaturedBooks() {
     // TODO: implement featchFeaturedBooks
     throw UnimplementedError();
   }
