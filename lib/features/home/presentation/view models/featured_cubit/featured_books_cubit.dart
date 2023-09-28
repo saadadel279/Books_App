462ec6898d19006b067a4f8a9883ee7f06d8d287
Your Name <you@example.com>
2023-09-28 09:40:03 +0300

    create featchFeaturedBookscubit func

--- lib/features/home/presentation/view models/featured_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/view models/featured_cubit/featured_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:books_app/features/home/data/models/book_model/book_model.dart';
+import 'package:books_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepoImp) : super(FeaturedBooksInitial());
+final HomeRepoImp homeRepoImp;
+  Future<void> featchFeaturedBooks()async{
+    emit(FeaturedBooksLoading());
+   var response=await homeRepoImp.featchFeaturedBooks();
+    response.fold((failure) {
+      emit(FeaturedBooksFaileur(failure.errMessage));
+    } , (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
